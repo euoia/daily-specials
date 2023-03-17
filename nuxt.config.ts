0885dca2092bc68624cc1c23620d146fe35eeec3
James Pickard <james.pickard@gmail.com>
2023-03-17 10:37:03 +0000

    Set page to A5

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["normalize.css/normalize.css", "~/app.scss"],
+  app: {
+    head: {
+      style: ["@page { size: A5 }>"]
+    }
+  }
 });
